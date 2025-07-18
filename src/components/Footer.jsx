69ab5c6a5739610db0b4d2e24855874367fe16ca
Atesh <73840304+Atesh1710@users.noreply.github.com>
2025-07-18 22:46:37 +0530

    changed company name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-primary text-white py-6 text-center">
-      <p>&copy; {new Date().getFullYear()} IronCast Co. | All Rights Reserved</p>
+      <p>&copy; {new Date().getFullYear()} NM MishrDhatu | All Rights Reserved</p>
     </footer>
   );
 }
